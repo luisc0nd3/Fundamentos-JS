1c3228d3bea195e7456f1905b7d54ba0fc7cbf93
conde <developer.luis.conde@gmail.com>
2021-07-04 23:35:26 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log('Hola mundo');
+
+let a = 10; // variable de bloque {}
+var b = 10; // variable global
+const c = 10; // es una constante
+
+console.log({a});
+console.log({b});
+console.log({c});
+
+console.table({a, b, c});
