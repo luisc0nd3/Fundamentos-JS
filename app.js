2fadbee343f42686fa8234ec31c654b1b9c9a335
conde <developer.luis.conde@gmail.com>
2021-07-05 00:07:36 -0500

    Impresion de tabla con variables

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 let a = 10; // variable de bloque {}
 var b = 10; // variable global
 const c = 10; // es una constante
+let d = 'Hola ',
+x = 'Conde ';
 
 console.log({a});
 console.log({b});
 console.log({c});
 
-console.table({a, b, c});
+console.table({a, b, c, d, x});
+
+const saludo = d + x;
