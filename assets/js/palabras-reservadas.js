85b473b982bd31ad177da87fc59f8b37bafbbd9f
conde <developer.luis.conde@gmail.com>
2021-07-08 02:09:46 -0500

    mas archivos de palabras reservadas

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,17 @@
+console.log(null);
+
+// Number
+
+// if
+
+// while
+
+// var for = 10;
+
+// for()
+
+// class juegoAño { // es mejor anio aunque deberia ser en ingles :P
+    
+// }
+
+let _object$ = ''; // definitivamente no usar nada de esto XD
---
+++ assets/js/primitivos.js
@@ -0,0 +1,27 @@
+
+let nombre = 'Peter Parcker';
+console.log('nombre: ' + nombre + ' is a ' + typeof nombre);
+
+nombre = 123;
+console.log('nombre: ' + nombre + ' is a ' + typeof nombre);
+
+nombre = 123.456;
+console.log('nombre: ' + nombre + ' is a ' + typeof nombre);
+
+
+let isMarvel = true;
+console.log('isMarvel: ' + isMarvel + ' is a ' + typeof isMarvel);
+
+let superPoderSpiderman;
+console.log('superPoderSpiderman: ' + superPoderSpiderman + ' is a ' + typeof superPoderSpiderman);
+
+let isNull = null;
+console.log('isNull: ' + isNull + ' is a ' + typeof isNull); // es un objeto
+
+let symbolOne = Symbol('a');
+console.log('symbolOne: ' + ' is a ' + typeof symbolOne);
+
+let symbolTwo = Symbol('a');
+console.log('symbolTwo: ' + ' is a ' + typeof symbolTwo);
+
+console.log(symbolOne == symbolTwo);  // <>
