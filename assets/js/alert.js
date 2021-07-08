c5b757b24e1e51b79c22ba59583e0ea3fee0ef85
conde <developer.luis.conde@gmail.com>
2021-07-08 00:39:28 -0500

    final deÃl primer capitulo

---
+++ assets/js/alert.js
@@ -0,0 +1,12 @@
+
+// alert('hola mundo!');
+
+// el prompt puede retornar un '' or null aparte del valor ingresado
+// let nombre = prompt('¿Cuál es tu nombre?');
+// console.log(nombre)
+// console.log('***' + nombre + '***');
+
+const option = confirm('¿Estas seguro de borrar el registro?');
+console.log(option);
+
+// console.log(global); //solo para node
