78c1c3fa14e0610a060703a8bb378a6b401f1572
conde <developer.luis.conde@gmail.com>
2021-08-11 23:37:36 -0500

    master | Pro-tip de operador ternario

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -12,3 +12,12 @@ openTime = ([0,6].includes( day ))? 9 : 11;
 msg = (timeDay >= openTime)? 'It´s Open': `It´s Close, today open around ${openTime}` ;
 
 console.log({openTime, msg});
+
+//operador ternario en función de flecha
+// const oldest = (a, b) => {
+//     return (a>b)? a: b;
+// }
+
+const oldest = (a, b) => (a>b)? a: b;
+
+console.log(oldest(20git , 15));
